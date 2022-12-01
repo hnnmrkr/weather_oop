59b19607de6dcc5d21b933b7b4f3bc088815e445
Hanna <hanna-marii.kriisa@voco.ee>
2022-12-01 11:04:54 +0200

    Added HTML and weather API function to display weather

---
+++ app.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', cityWeather)
+
+function weatherDataFetch(city) {
+    var key = 'd4704927e8828d83ba4cc33b4483e308';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then(function(resp){
+            return resp.json()
+        })
+        .then(function(data) {
+            console.log(data)
+            drawWeather(data)
+        })
+        .catch(function(){
+        });
+}
+
+function cityWeather(e){
+    weatherDataFetch('Tõrvandi');
+}
+
+function drawWeather( data ) {
+    var celcius = Math.round(parseFloat(data.main.temp)-273.15);
+    var description = data.weather[0].description;
+
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML = celcius + '&deg;';
+    document.querySelector('#location').innerHTML = data.name;
+}
